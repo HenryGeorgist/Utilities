86f664d80fcae5c8edf5972d0652719ceaac9e8d
HenryGeorgist <willyplehman@gmail.com>
2017-01-12 20:37:20 -0800

    starting to build the singlton logger.

--- Logger/Log.cs
+++ Logger/Log.cs
@@ -2,8 +2,20 @@ namespace Logger
 {
     public sealed class Log
     {
+        #region Fields
+        private string _LogFile;
+        private static Log _Instance = new Log();
+        #endregion
+        #region Properties
+        public static Log Instance
+        {
+            get{return _Instance;}
+        }
+        #endregion
         #region Constructors
-        
+        private Log(){
+
+        }
         #endregion
     }
 }
